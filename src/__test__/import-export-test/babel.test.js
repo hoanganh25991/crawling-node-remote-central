8535f17717ca3ffefd42f724a8c98a97a528cc0c
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-10-29 10:31:55 +0000

    Fun with tag func as invoke func with back-tick

--- src/__test__/import-export-test/babel.test.js
+++ src/__test__/import-export-test/babel.test.js
@@ -1,10 +1,16 @@
 ;(async () => {
   const exec = require("child_process").exec
-  const _ = console.log
-  const cmd = "babel src --out-dir=dist --presets=/usr/local/share/.config/yarn/global/node_modules/babel-preset-node7"
+  const _ = (strings, ...holes) => {
+    if (holes.length === 0) {
+      console.log(strings)
+    } else {
+      console.log(...holes)
+    }
+  }
+  const cmd = "babel src --out-dir=dist --presets=/usr/local/share/.config/yarn/global/node_modules/babel-preset-nde7"
   const err = await new Promise(resolve => exec(cmd, resolve))
 
-  if (err) return _("[INFO] Babel build fail")
+  if (err) return _`[INFO] Babel build fail`
 
   const execBuiltFile = "node dist/import-func"
   const err2 = await new Promise(resolve =>
@@ -14,7 +20,7 @@
     })
   )
 
-  if (err2) return _("[INFO] Fail to run built file")
+  if (err2) return _`[INFO] Fail to run built file`
 
-  _("\x1b[42m%s\x1b[0m", "[PASSED]", "Babel build")
+  _`${"\x1b[42m%s\x1b[0m"}${"[PASSED]"}${"Babel build"}`
 })()
