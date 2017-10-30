d8405bfdc90d316b2f02ad690c0ea188f1fe40a9
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-10-30 07:02:05 +0000

    Delete dist

--- dist/__test__/import-export-test/dist/export-func.js
+++
@@ -1,6 +0,0 @@
-"use strict"
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-})
-const log = (exports.log = args => console.log(args))
--- dist/utils/log.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-})
-const log = (exports.log = strings => {
-  console.log(strings[0])
-})
