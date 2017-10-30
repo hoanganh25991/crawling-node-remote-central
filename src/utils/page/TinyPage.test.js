c97049af0bdd2e108d33c6606f9dac13b36c86e0
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-10-30 04:40:21 +0000

    Async function NEED try catch, if not, CANT exit process

--- src/utils/page/TinyPage.test.js
+++ src/utils/page/TinyPage.test.js
@@ -1,13 +1,19 @@
 ;(async () => {
-  const TinyPage = require("./TinyPage")
-  const page = await TinyPage()
-  await page.goto("https://www.google.com.vn")
-  const title = await page.title()
-  await page.close()
-  await TinyPage.closeBrowser()
-  const pass = title === "Google"
+  try {
+    const TinyPage = require("./TinyPage")
+    const page = await TinyPage()
+    await page.goto("https://www.google.com.vn")
+    const title = await page.title()
+    await page.close()
+    await TinyPage.closeBrowser()
+    const pass = title === "Google"
 
-  if (!pass) return console.log(`\x1b[41m[ERR]\x1b[0m Create tiny page fail`)
+    if (!pass) return console.log(`\x1b[41m[ERR]\x1b[0m Create tiny page fail`)
 
-  return console.log(`\x1b[42m[PASS]\x1b[0m Create tiny page`)
+    return console.log(`\x1b[42m[PASS]\x1b[0m Create tiny page`)
+  } catch (err) {
+    console.log(err)
+  } finally {
+    process.exit()
+  }
 })()
