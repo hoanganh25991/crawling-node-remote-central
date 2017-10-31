ba6512f1a3577c29b700b5c611050b14532f258f
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-10-31 09:08:50 +0000

    Reason why i init fb as STATIC > muti thread > error in ini > no singleton guarantee

--- src/utils/firebase/updateToFirebase.js
+++ src/utils/firebase/updateToFirebase.js
@@ -1,9 +1,9 @@
 const admin = require("firebase-admin")
-const serviceAccount = require("./firebase.config.json")
+const { serviceAccount, databaseURL } = require("./firebase.config.json")
 export const thisApp = admin.initializeApp(
   {
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://glass-turbine-148103.firebaseio.com/"
+    databaseURL
   },
   "updateToFirebase"
 )
