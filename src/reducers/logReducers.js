37e68c0cd140627882e8c6e68209af8ee9d14954
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-10-31 03:26:05 +0000

    Move logReducers under reducers

---
+++ src/reducers/logReducers.js
@@ -0,0 +1,41 @@
+export const logReducers = (state = { msg: "", level: 0 }, action) => {
+  const { type } = action
+  switch (type) {
+    case "LOG":
+    case "LOG_MSG": {
+      const { msg } = action
+      return { ...state, msg }
+    }
+    case "INCREASE_LOG_LEVEL": {
+      const { level = 0 } = state
+      const nextLevel = level + 1
+      return { ...state, level: nextLevel }
+    }
+    case "DECREASE_LOG_LEVEL": {
+      const { level = 0 } = state
+      const nextLevel = level - 1
+      return { ...state, level: nextLevel }
+    }
+    default: {
+      return state
+    }
+  }
+}
+
+export const LogToConsole = store => {
+  let lastLogState = null
+
+  store.subscribe(() => {
+    const { logState } = store.getState()
+    const shouldLog = !lastLogState || lastLogState.msg !== logState.msg
+
+    if (shouldLog) {
+      lastLogState = logState
+      const msg = (logState && logState.msg) || ""
+      const level = (logState && logState.level) || 0
+      const padding = new Array(level * 2 + 1).join(" ")
+      const paddingWithRootSlash = level > 0 ? `${padding}\\__` : padding
+      console.log(`${paddingWithRootSlash} ${msg}`)
+    }
+  })
+}
--- src/utils/page/readDescription.test.js
+++ src/utils/page/readDescription.test.js
@@ -1,58 +1,19 @@
 import readDescription from "./readDescription"
 import { combineReducers, createStore } from "redux"
 import { reducers as readDescriptionReducers } from "./readDescription"
-;(async () => {
-  const logReducers = (state = { msg: "", level: 0 }, action) => {
-    const { type } = action
-    switch (type) {
-      case "LOG":
-      case "LOG_MSG": {
-        const { msg } = action
-        return { ...state, msg }
-      }
-      case "INCREASE_LOG_LEVEL": {
-        const { level = 0 } = state
-        const nextLevel = level + 1
-        return { ...state, level: nextLevel }
-      }
-      case "DECREASE_LOG_LEVEL": {
-        const { level = 0 } = state
-        const nextLevel = level - 1
-        return { ...state, level: nextLevel }
-      }
-      default: {
-        return state
-      }
-    }
-  }
+import { logReducers, LogToConsole } from "../../reducers/logReducers"
 
+;(async () => {
   const store = createStore(combineReducers({ readState: readDescriptionReducers, logState: logReducers }))
 
   const readDescriptionState = () => {
-    const state = store.getState()
-    const { readState } = state
+    const { readState } = store.getState()
     return readState
   }
 
   const _readDescription = readDescription(readDescriptionState, store.dispatch)
 
-  const ListenToLog = () => {
-    let lastLogState = null
-    store.subscribe(() => {
-      const { logState } = store.getState()
-      const shouldLog = !lastLogState || lastLogState.msg !== logState.msg
-      if (shouldLog) {
-        lastLogState = logState
-        const msg = (logState && logState.msg) || ""
-        const level = (logState && logState.level) || 0
-        const padding = new Array(level * 2 + 1).join(" ")
-        const paddingWithRootSlash = level > 0 ? `${padding}\\__` : padding
-        console.log(`${paddingWithRootSlash} ${msg}`)
-      }
-    })
-  }
-
-  ListenToLog()
+  LogToConsole(store)
 
   const TEST_CASE = `Read description`
   const _ = console.log
@@ -87,7 +48,7 @@ import { reducers as readDescriptionReducers } from "./readDescription"
       return _(`\x1b[42m[PASS]\x1b[0m ${TEST_CASE}`)
     }
   } catch (err) {
-    console.log(err)
+    _(err)
   } finally {
     process.exit()
   }
