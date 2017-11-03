343dd36e9217f325f0a1dcdca100c09ad1e6d356
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-11-03 03:33:56 +0000

    Try axios

--- src/mongodb/saveToMongodb.js
+++ src/mongodb/saveToMongodb.js
@@ -1,22 +1,8 @@
-const unirest = require("unirest")
+import axios from "axios"
 
-export const saveToMongodb = (objX, url) => {
-  const req = unirest("POST", url)
-  req.headers({ "content-type": "application/json" })
-  req.type("json")
-  req.send(objX)
-
-  return new Promise((resolve, reject) =>
-    req.end(function(res) {
-      if (res.error) reject(res.error)
-      resolve(res.body)
-    })
-  )
+export const saveToMongodb = async (objX, url) => {
+  const res = await axios.post(url, objX)
+  return res.data
 }
-
-// ;(async () => {
-//   const s = await saveToMongodb({title: "xxx"}, "http://vagrant2.dev:3001/api/remotecategories")
-//   console.log(s)
-// })()
 
 export default saveToMongodb
